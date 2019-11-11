70cc3b9fbbb019b874461ab790de2a5b0544b07e
Alphius Wambua <alphiuswambua@gmail.com>
2019-11-11 10:14:34 +0300

    Part 9 updating and deleting addresses

--- core/urls.py
+++ core/urls.py
@@ -1,29 +1,32 @@
 from django.urls import path
 from .views import (
+    UserIDView,
+    ItemListView,
     ItemDetailView,
-    CheckoutView,
-    HomeView,
-    OrderSummaryView,
-    add_to_cart,
-    remove_from_cart,
-    remove_single_item_from_cart,
+    AddToCartView,
+    OrderDetailView,
     PaymentView,
     AddCouponView,
-    RequestRefundView
+    CountryListView,
+    AddressListView,
+    AddressCreateView,
+    AddressUpdateView,
+    AddressDeleteView
 )
 
-app_name = 'core'
-
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('user-id/', UserIDView.as_view(), name='user-id'),
+    path('countries/', CountryListView.as_view(), name='country-list'),
+    path('addresses/', AddressListView.as_view(), name='address-list'),
+    path('addresses/create/', AddressCreateView.as_view(), name='address-create'),
+    path('addresses/<pk>/update/',
+         AddressUpdateView.as_view(), name='address-update'),
+    path('addresses/<pk>/delete/',
+         AddressDeleteView.as_view(), name='address-delete'),
+    path('products/', ItemListView.as_view(), name='product-list'),
+    path('products/<pk>/', ItemDetailView.as_view(), name='product-detail'),
+    path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
+    path('order-summary/', OrderDetailView.as_view(), name='order-summary'),
+    path('checkout/', PaymentView.as_view(), name='checkout'),
     path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
-         name='remove-single-item-from-cart'),
-    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
-    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
 ]
